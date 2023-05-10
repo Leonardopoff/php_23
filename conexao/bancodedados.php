b3dd58be856e4160d83692fd046644870aa3eba9
leonardo <leonardopiva26@gmail.com>
2023-05-09 21:11:41 -0300

    Aula

--- conexao/bancodedados.php
+++ conexao/bancodedados.php
@@ -4,17 +4,21 @@ $hostname = "localhost";
 $username = "aluno";
 $password = "qwe123!";
 $port= 3306;
+$database = 'aluno';
 
 try {
-    $connection = mysqli_connect(
+    $conexaoBanco = mysqli_connect(
         $hostname,
         $username,
         $password,
         $database,
         $port
     );
+
+    echo "Conectou!!!";
 } catch (Exception $error ){
     echo "ERRO codigo: {$error->getCode()} <br> mensagem: {$error->getMessage()}";
     // echo "ERRO codigo: " . $error->getCode() . " <br> mensagem: " . $error->getMessage();
     // echo "ERRO: {$error}";
+    $conexaoBanco = null;
 }
--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,10 +1,32 @@
 <?php
 
-class BaseModel {
-    public $bancoSelecionado;
+// CRUD----->> Creat,Read,Update, Delete.
+
+require_once '../conexao/bancodedados.php'; // vai carregar uma vez
+// require vai carregar novamente
+
+//include '../conexao/bancodedados.php'; vai carregar sempre que executado
+//include_once '../conexao/bancodedados.php'; vai carregar uma vez
+
+abstract class BaseModel {
+    public $conexaoBancoBM;
     
-    public function __construct() {
+    public function __construct( $conexaoBancoParam = null) {
         // uses database php_lab
-        $this->bancoSelecionado = "php_23";
+        $this->$conexaoBancoBM = $conexaoBancoParam;
     }
+
+
+    public function getConexao (){
+        var_dump($this -> $conexaoBancoBM);
+
+    }
+
 }
+$baseModel =  new BaseModel($conexaoBanco);
+//$baseModel -> conexaoBancoBM;
+$BaseModel -> getConexao();
+
+
+
+echo " ola Mundo";
